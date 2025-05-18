46783aa58d269457830ee46ec41237543efd81ce
metaknowledge <danielh5@uw.edu>
2025-05-17 19:10:58 -0700

    done2

--- main.py
+++ main.py
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-import pygame
-from pygame.locals import *
-
-from src.components.game_state import GameState
-from src.global_state import GlobalState
-from src.game_phase import main_menu, game, settings
-from src.services.visualization_service import VisualizationService
-from src.services.UI_service import UIService
-from src.components.interface_node import InterfaceNode
-from src.custom_events import CustomEvents
-from src.utils.game_tools import load_planets
-from src.utils.tools import add_tuples
-from src.components.player import Player
-
-import logging
-logging.basicConfig(filename='logs.log',filemode="w", encoding='utf-8', level=logging.DEBUG)
-
-
-
-def main():
-  # start pygame
-  pygame.init()
-  GlobalState.load_window()
-  screen_sizes = pygame.display.get_desktop_sizes()
-  logging.debug(screen_sizes)
-
-  GlobalState.load_user_interface()
-
-  GlobalState.player = Player(pygame.Vector2(21000, 0), "white", 0.1, pygame.Vector2(0, -1), pygame.Vector2(0, -1))
-
-  # game loop
-  while GlobalState.running:
-    if GlobalState.GAME_STATE == GameState.MAIN_MENU:
-      main_menu()
-    if GlobalState.GAME_STATE == GameState.SETTINGS:
-      settings()
-    if GlobalState.GAME_STATE == GameState.GAME:
-      game()
-
-    GlobalState.SCREEN.fill("white")
-
-
-
-    GlobalState.world_ui.update()
-    GlobalState.world_ui.draw(GlobalState.SCREEN)
-
-    GlobalState.update_game_display()
-
-
-  pygame.quit()
-
-
-
-if __name__ == "__main__":
-=======
 import pygame
 from pygame.locals import *
 
@@ -105,5 +49,4 @@ def main():
 
 
 if __name__ == "__main__":
->>>>>>> caa9f23c1f0a44c7ce068ce12609939477b9d911
     main()
--- src/utils/tools.py
+++ src/utils/tools.py
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import pygame
-from pygame.locals import *
-import logging
-from src.custom_events import CustomEvents
-
-def post_quit():
-  pygame.event.post(pygame.event.Event(QUIT))
-  logging.debug("exit")
-
-def change_screen_size(size: tuple):
-  pygame.display.set_mode(size)
-  CustomEvents.post(CustomEvents.CHANGE_SIZE)
-  logging.info(size)
-
-# replaces old_node with new_node in group
-def replace_node(group, old_node, new_node):
-  group.remove(old_node)
-  group.add(new_node)
-
-def add_tuples(a: tuple, b: tuple) -> tuple:
-=======
 import pygame
 from pygame.locals import *
 import logging
@@ -39,5 +17,4 @@ def replace_node(group, old_node, new_node):
   group.add(new_node)
 
 def add_tuples(a: tuple, b: tuple) -> tuple:
->>>>>>> caa9f23c1f0a44c7ce068ce12609939477b9d911
   return list(map(sum, zip(a, b)))
